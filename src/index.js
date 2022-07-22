90195a3f6c87d5f206e962bb30a842f106063f3e
dapv67 <alvarodperezv@gmail.com>
2022-07-22 10:06:29 -0500

    Listos para produccion V1

--- src/index.js
+++ src/index.js
@@ -18,5 +18,8 @@ app.use(require("./routes/index"));
 //Static Content
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000);
-console.log("Server listening...");
+const PORT = process.env.PORT || 3000; //Variable de entorno: Variable que del entorno donde se esta ejecutando el codigo
+app.listen(3000, function () {
+  console.log("Servidor escuchando en el puerto", PORT);
+});
+// console.log("Server listening...");
